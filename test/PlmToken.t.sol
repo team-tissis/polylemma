8a1d70265a0143e22dbd5d277aae68f0423ed695
shiinokinoki <shue.go.go@gmail.com>
2022-10-24 16:40:04 +0900

    MOD: PLMToken test

--- test/PlmToken.t.sol
+++ test/PlmToken.t.sol
@@ -11,7 +11,7 @@ import {IPLMData} from "../src/interfaces/IPLMData.sol";
 
 contract PLMTokenTest is Test {
     // EOA
-    address minter = address(101);
+    address minter = msg.sender;
 
     PLMSeeder seederContract;
     PLMData dataContract;
@@ -25,11 +25,50 @@ contract PLMTokenTest is Test {
         seederContract = new PLMSeeder();
         seeder = IPLMSeeder(address(seederContract));
         data = IPLMData(address(dataContract));
-        token = new PLMToken(minter, seeder, data, 10000);
+        uint256 maxSupply = 10000;
+        token = new PLMToken(minter, seeder, data, maxSupply);
+    }
+
+    function testMint() public {
+        vm.prank(minter);
+        token.mint();
     }
 
     function testFailMintByNonMiner() public {
-        vm.prank(address(0));
+        vm.prank(address(10)); //set a diffrent address from minter's address to msg.sender
         token.mint();
+    }
+
+    function testTokenIdIncrement() public {
+        vm.startPrank(minter);
+        uint256 aTokenId = token.totalSupply();
+        token.mint();
+        uint256 nextTokenId = token.totalSupply();
+        vm.stopPrank();
+
+        assertEq(aTokenId + 1, nextTokenId);
+    }
+
+    function testIsCorrectOwnerMint() public {
+        vm.startPrank(minter);
+        token.mint();
+        uint256 latestTokenId = token.totalSupply();
+        address mintedTokenOwner = token.ownerOf(latestTokenId);
+        vm.stopPrank();
+
+        assertEq(mintedTokenOwner, minter);
+    }
+
+    function testCharacterInfo() public {
+        vm.startPrank(minter);
+        uint256 tokenId = token.mint();
+        PLMToken.CharacterInfo memory tokenInfo = token.getCharacterInfo(
+            tokenId
+        );
+        // check characterInfo initialization
+
+        // TODO: to test other members
+        assertEq(tokenInfo.level, 1);
+        vm.stopPrank();
     }
 }
