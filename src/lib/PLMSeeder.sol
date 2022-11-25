dfee540a5f0b7d144bce60d9b7d664ade510d770
shue_shiinoki <shue.go.go@gmail.com>
2022-11-25 16:22:14 +0900

    MOD: refactor seeder

--- src/lib/PLMSeeder.sol
+++ src/lib/PLMSeeder.sol
@@ -11,28 +11,58 @@ library PLMSeeder {
         uint8 attribute;
     }
 
+    function _randomFromTokenId(uint256 tokenId)
+        internal
+        view
+        returns (uint256)
+    {
+        return
+            uint256(
+                keccak256(
+                    abi.encodePacked(blockhash(block.number - 1), tokenId)
+                )
+            );
+    }
+
+    /// @notice Only the cumulativeOdds stores cumulative probabilities.
+    /// This function calc Id from the array with pseudoRandomness
+    function _matchRandomnessWithOdds(
+        uint256 pseudoRandomness,
+        uint8[] memory cumulativeOdds
+    ) internal pure returns (uint8) {
+        uint8 sumOdds = cumulativeOdds[cumulativeOdds.length - 1];
+        uint256 p = pseudoRandomness % sumOdds;
+        if (p < cumulativeOdds[0]) return 0;
+        for (uint8 i = 1; i < cumulativeOdds.length; i++) {
+            if (cumulativeOdds[i - 1] <= p && p < cumulativeOdds[i]) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    //////////////////////////
+    ///  SEEDER FUNCTIONS  ///
+    //////////////////////////
+
     /// @notice generate seeds for character mint
     /// @dev generate seeds of traits from current-block's hash for to mint character
     /// @param tokenId tokenId to be minted
     /// @return Seed the struct of trait seed that is indexId of trait array
-    function generateSeed(uint256 tokenId, IPLMData data)
+    function generateTokenSeed(uint256 tokenId, IPLMData data)
         external
         view
         returns (Seed memory)
     {
         // TODO: 画像は属性や特性と比較して総数が多いため、現行の実装を踏襲するとものによって排出確率を変更する実装が汚くなってしまうから、一旦一様分布で対応する。
-        uint256 pseudoRandomnessImg = _generateRandomnessFromBlockHash(tokenId);
+        uint256 pseudoRandomnessImg = _randomFromTokenId(tokenId);
         uint256 numImg = data.getNumImg();
 
-        uint256 pseudoRandomnessType = _generateRandomnessFromBlockHash(
-            tokenId + 1
-        );
+        uint256 pseudoRandomnessType = _randomFromTokenId(tokenId + 1);
         uint8[] memory cumulativeCharacterTypeOdds = data
             .getCumulativeCharacterTypeOdds();
 
-        uint256 pseudoRandomnessAttribute = _generateRandomnessFromBlockHash(
-            tokenId + 2
-        );
+        uint256 pseudoRandomnessAttribute = _randomFromTokenId(tokenId + 2);
         uint8[] memory cumulativeAttributeOdds = data
             .getCumulativeAttributeOdds();
 
@@ -52,23 +82,13 @@ library PLMSeeder {
 
     /// @notice generate nonce to be used as input of hash for randomSlotTokenId
     /// @dev generate nonce to be used as input of hash for randomSlotTokenId
-    /// TODO: 関数名変える
-    function generateRandomSlotNonce() external view returns (bytes32) {
+    function randomFromBlockHash() external view returns (bytes32) {
         return keccak256(abi.encodePacked(blockhash(block.number - 1)));
     }
 
-    function _generateRandomnessFromBlockHash(uint256 tokenId)
-        internal
-        view
-        returns (uint256)
-    {
-        return
-            uint256(
-                keccak256(
-                    abi.encodePacked(blockhash(block.number - 1), tokenId)
-                )
-            );
-    }
+    ////////////////////////
+    ///      GETTERS     ///
+    ////////////////////////
 
     /// @notice create tokenId for randomslot without being searched
     /// @dev create tokenId for randomslot without being searched
@@ -83,22 +103,5 @@ library PLMSeeder {
             keccak256(abi.encodePacked(nonce, playerSeed))
         ) % totalSupply) + 1;
         return tokenId;
-    }
-
-    /// @notice Only the cumulativeOdds stores cumulative probabilities.
-    /// This function calc Id from the array with pseudoRandomness
-    function _matchRandomnessWithOdds(
-        uint256 pseudoRandomness,
-        uint8[] memory cumulativeOdds
-    ) internal pure returns (uint8) {
-        uint8 sumOdds = cumulativeOdds[cumulativeOdds.length - 1];
-        uint256 p = pseudoRandomness % sumOdds;
-        if (p < cumulativeOdds[0]) return 0;
-        for (uint8 i = 1; i < cumulativeOdds.length; i++) {
-            if (cumulativeOdds[i - 1] <= p && p < cumulativeOdds[i]) {
-                return i;
-            }
-        }
-        return 0;
     }
 }
